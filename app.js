6e90c8b8ba1a89e6dd0be3a4de5d09a8130a890c
Amrith Gandham <amrithgandham@gmail.com>
2025-06-02 19:12:26 -0700

    Update starting page

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public', 'league.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
     req.models = models;
